83e9aa90c0f2469c221e4897cdbb25934eeba80c
Ilya Esenko <ilya.esenko@gmail.com>
2024-03-02 22:39:49 +0300

    add polymorphic button

---
+++ src/components/ui/button/button.stories.ts
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button } from './'
+
+const meta = {
+  component: Button,
+  tags: ['autodocs'],
+  title: 'Components/Button',
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    children: 'Primary Button',
+    disabled: false,
+    variant: 'primary',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    children: 'Secondary Button',
+    disabled: false,
+    variant: 'secondary',
+  },
+}
+
+export const FullWidth: Story = {
+  args: {
+    children: 'Full Width Primary Button',
+    disabled: false,
+    fullWidth: true,
+    variant: 'primary',
+  },
+}
+
+export const AsLink: Story = {
+  args: {
+    as: 'a',
+    children: 'Link that looks like a button',
+    variant: 'primary',
+  },
+}
---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+import s from './button.module.scss'
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  fullWidth?: boolean
+  variant?: 'primary' | 'secondary'
+} & ComponentPropsWithoutRef<T>
+
+export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+  const {
+    as: Component = 'button',
+    className,
+    fullWidth = false,
+    variant = 'primary',
+    ...rest
+  } = props
+
+  return (
+    <button className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''}`} {...rest} />
+  )
+}
