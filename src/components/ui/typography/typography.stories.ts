982b2e3ec61247bd69b9b4def589050a17bb0eda
Neakhai Yauheni <beonezero@protonmail.com>
2024-03-04 23:12:25 +0300

    pnpm run lint

--- src/components/ui/typography/typography.stories.ts
+++ src/components/ui/typography/typography.stories.ts
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Typography } from './'
-import { TypographyVariant } from "@/components/ui/typography/enum";
+import { TypographyVariant } from '@/components/ui/typography/enum'
 
+import { Typography } from './'
 
 const meta = {
   component: Typography,
@@ -15,81 +15,81 @@ type Story = StoryObj<typeof meta>
 
 export const H1: Story = {
   args: {
+    children: 'H1. Text',
     variant: TypographyVariant.h1,
-    children: 'H1. Text'
-  }
+  },
 }
 export const H2: Story = {
   args: {
+    children: 'H2. Text',
     variant: TypographyVariant.h2,
-    children: 'H2. Text'
-  }
+  },
 }
 export const H3: Story = {
   args: {
+    children: 'H3. Text',
     variant: TypographyVariant.h3,
-    children: 'H3. Text'
-  }
+  },
 }
 export const H4: Story = {
   args: {
+    children: 'H4. Text',
     variant: TypographyVariant.h4,
-    children: 'H4. Text'
-  }
+  },
 }
 
 export const Body1: Story = {
   args: {
+    children: 'Body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
     variant: TypographyVariant.body1,
-    children: 'Body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-  }
+  },
 }
 
 export const Body2: Story = {
   args: {
-    variant: TypographyVariant.body2,
-    children: 'Body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
-  }
+    children: 'Body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    variant: TypographyVariant.body1,
+  },
 }
 
 export const Caption: Story = {
   args: {
+    children: 'Caption. Any text.',
     variant: TypographyVariant.caption,
-    children: 'Caption. Any text.'
-  }
+  },
 }
 
 export const Link1: Story = {
   args: {
+    children: 'Link1. https://it-incubator.io/',
     variant: TypographyVariant.link1,
-    children: 'Link1. https://it-incubator.io/'
-  }
+  },
 }
 
 export const Link2: Story = {
   args: {
+    children: 'Link2. https://it-incubator.io/',
     variant: TypographyVariant.link2,
-    children: 'Link2. https://it-incubator.io/'
-  }
+  },
 }
 
 export const Overline: Story = {
   args: {
+    children: 'Overline. Some text',
     variant: TypographyVariant.overline,
-    children: 'Overline. Some text'
-  }
+  },
 }
 
 export const Subtitle1: Story = {
   args: {
+    children: 'Subtitle1',
     variant: TypographyVariant.subtitle1,
-    children: 'Subtitle1'
-  }
+  },
 }
 
 export const Subtitle2: Story = {
   args: {
+    children: 'Subtitle2',
     variant: TypographyVariant.subtitle2,
-    children: 'Subtitle2'
-  }
+  },
 }
