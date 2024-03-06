7db9880811424e966216190e54bac2fff8ab6ab3
ekameyshaaa <ekameyshaaa@gmail.com>
2024-03-06 07:49:31 +0300

    dropdown item style refactor

--- src/components/ui/dropdown/dropdown.tsx
+++ src/components/ui/dropdown/dropdown.tsx
@@ -5,31 +5,34 @@ import Player from "@/assets/icons/player";
 
 import {TypographyVariant} from "@/components/ui/typography/enum";
 import {Typography} from "@/components/ui/typography";
+
 const Dropdown = () => {
     return (
-        <DropdownMenu.Root  open={true}>
+        <DropdownMenu.Root>
             <DropdownMenu.Trigger asChild>
-                <button className={s.IconButton } aria-label="Customise options">
-                 <Outside/>
+                <button className={s.IconButton} aria-label="Customise options">
+                    <Outside/>
                 </button>
             </DropdownMenu.Trigger>
             <DropdownMenu.Portal>
                 <DropdownMenu.Content side={'bottom'} className={s.dropdownMenuContent}>
-                    <DropdownMenu.Item  className={s.dropdownMenuItem}>
-                        <span className={s.dropdownMenuItemSpan}><Player/> </span><Typography   variant={TypographyVariant.body1}  children={'Learn'}/>
+                    <DropdownMenu.Item className={s.dropdownMenuItem}>
+                        <span className={s.dropdownMenuItemSpan}><Player/> </span><Typography
+                        style={{marginTop: '0', marginBottom: '0'}} variant={TypographyVariant.body1}
+                        children={'Learn'}/>
                     </DropdownMenu.Item>
                     <DropdownMenu.Separator className={s.dropdownSeparator}/>
-                    <DropdownMenu.Item  className={s.dropdownMenuItem}>
-                        <span className={s.dropdownMenuItemSpan}><Player/> </span><Typography   variant={TypographyVariant.caption}  children={'Edit'}/>
+                    <DropdownMenu.Item className={s.dropdownMenuItem}>
+                        <span className={s.dropdownMenuItemSpan}><Player/> </span><Typography
+                        style={{marginTop: '0', marginBottom: '0'}} variant={TypographyVariant.body1}
+                        children={'Edit'}/>
                     </DropdownMenu.Item>
-                    <DropdownMenu.Separator className={s.dropdownSeparator} />
-                    <DropdownMenu.Item  className={s.dropdownMenuItem}>
-                        <span className={s.dropdownMenuItemSpan}><Player/> </span><Typography   variant={TypographyVariant.caption}  children={'Delete'}/>
+                    <DropdownMenu.Separator className={s.dropdownSeparator}/>
+                    <DropdownMenu.Item className={s.dropdownMenuItem}>
+                        <span className={s.dropdownMenuItemSpan}><Player/> </span><Typography
+                        style={{marginTop: '0', marginBottom: '0'}} variant={TypographyVariant.body1}
+                        children={'Delete'}/>
                     </DropdownMenu.Item>
-
-                    {/*<DropdownMenu.Item  className={s.dropdownMenuItem}>*/}
-                    {/* <Player/> <Typography   variant={TypographyVariant.caption}  children={'fkkfkf djdjjdjdnfnjejfnejefjefjefnjfnejjnfnenf'}/>*/}
-                    {/*</DropdownMenu.Item>*/}
                 </DropdownMenu.Content>
             </DropdownMenu.Portal>
         </DropdownMenu.Root>
