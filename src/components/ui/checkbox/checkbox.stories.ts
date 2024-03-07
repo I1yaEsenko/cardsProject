a4eb09f79232682738346d11f021045591cae428
Neakhai Yauheni <beonezero@protonmail.com>
2024-03-07 23:10:23 +0300

    refactoring checkbox.stories.ts

--- src/components/ui/checkbox/checkbox.stories.ts
+++ src/components/ui/checkbox/checkbox.stories.ts
@@ -1,42 +1,42 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { Checkbox } from "./";
+import { Checkbox } from './'
 
 const meta = {
   component: Checkbox,
-  tags: ["autodocs"],
-  title: "Components/Checkbox"
-} satisfies Meta<typeof Checkbox>;
+  tags: ['autodocs'],
+  title: 'Components/Checkbox',
+} satisfies Meta<typeof Checkbox>
 
-export default meta;
+export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {}
-};
+  args: {},
+}
 
 export const Checked: Story = {
   args: {
-    checked: true
-  }
-};
+    checked: true,
+  },
+}
 
 export const UnChecked: Story = {
   args: {
-    checked: false
-  }
-};
+    checked: false,
+  },
+}
 
 export const WithLabel: Story = {
   args: {
-    label: "Some text"
-  }
-};
+    label: 'Some text',
+  },
+}
 
 export const Disabled: Story = {
   args: {
     checked: true,
     disabled: true,
-    label: "SomeText"
-  }
-};
+    label: 'SomeText',
+  },
+}
