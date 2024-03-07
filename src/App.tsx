329c53a62f9e0b2982a65d15178d35829d8c566b
ekameyshaaa <ekameyshaaa@gmail.com>
2024-03-07 23:39:47 +0300

    dropdown universal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-// import Dropdown from "@/components/ui/dropdown/dropdown";
-import DropdownUser from "@/components/ui/dropdown/dropdownUser/dropdownUser";
 
 export function App() {
-  return <div style={{display:'flex', justifyContent:'flex-end'}}><DropdownUser/></div>
+  return <div>App</div>
 }
