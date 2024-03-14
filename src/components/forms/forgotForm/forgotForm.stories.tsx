350298249838757e8fb9787d54bce9955d329a7a
ekameyshaaa <ekameyshaaa@gmail.com>
2024-03-14 21:15:48 +0300

    forgotForm

---
+++ src/components/forms/forgotForm/forgotForm.stories.tsx
@@ -0,0 +1,14 @@
+import {Meta, StoryObj} from "@storybook/react";
+import ForgotForm from "@/components/forms/forgotForm/forgotForm";
+
+const meta = {
+    component: ForgotForm,
+    tags: ['autodocs'],
+    title: 'Components/ForgotForm',
+} satisfies Meta<typeof ForgotForm>
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const ForgotForm1: Story ={
+
+}
