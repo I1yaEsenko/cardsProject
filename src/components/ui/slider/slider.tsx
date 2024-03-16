bf71801a7b364de909784619641d5189422500c1
Neakhai Yauheni <beonezero@protonmail.com>
2024-03-16 21:05:33 +0300

    merge slider branch and add comets slider.tsx

--- src/components/ui/slider/slider.tsx
+++ src/components/ui/slider/slider.tsx
@@ -11,9 +11,25 @@ type Props = {
   value?: number[]
 } & Omit<ComponentPropsWithoutRef<typeof SliderRadix.Slider>, 'asChild'>
 
+/**
+ * Компонент Slider.
+ *
+ * Этот компонент Slider является оберткой над компонентом Slider из '@radix-ui/react-slider'.
+ * Он предоставляет дополнительные функции и стилизацию.
+ *
+ * @param {Props} props - Свойства компонента Slider.
+ * @param {string} props.className - Дополнительные CSS-классы для компонента Slider.
+ * @param {number} props.max - Максимальное значение для слайдера.
+ * @param {Function} props.onValueChange - Функция обратного вызова, вызываемая при изменении значения слайдера.
+ * @param {number[]} props.value - Текущее значение слайдера.
+ * @param {...any} rest - Дополнительные свойства для компонента Slider.
+ * @returns {JSX.Element} - Рендер компонента Slider.
+ */
+
 export const Slider = forwardRef<ElementRef<typeof SliderRadix.Slider>, Props>(
   ({ className, max, onValueChange, value, ...rest }, ref) => {
     useEffect(() => {
+      // Установка значения по умолчанию для второго ползунка, если ононе определено или равно null
       if (value?.[1] === undefined || value?.[1] === null) {
         onValueChange?.([value?.[0] ?? 0, max ?? 0])
       }
