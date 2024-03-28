f731853cae8f2330dddabd0da86c173abfc00a98
Neakhai Yauheni <beonezero@protonmail.com>
2024-03-28 23:29:34 +0300

    crate test prototype pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import { SetStateAction, useMemo, useState } from "react";
+import data from "./mock-data.json";
+import Pagination from "@/components/ui/pagintion/pagination";
 
+let PageSize = 10;
 
-export function App() {
-  return <div>Cards project </div>
+export const  App = () => {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const currentTableData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return data.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
+
+  return (
+    <>
+      <table>
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>FIRST NAME</th>
+          <th>LAST NAME</th>
+          <th>EMAIL</th>
+          <th>PHONE</th>
+        </tr>
+        </thead>
+        <tbody>
+        {currentTableData.map(item => {
+          return (
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.first_name}</td>
+              <td>{item.last_name}</td>
+              <td>{item.email}</td>
+              <td>{item.phone}</td>
+            </tr>
+          );
+        })}
+        </tbody>
+      </table>
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={data.length}
+        pageSize={PageSize}
+        onPageChange={(page: SetStateAction<number>) => setCurrentPage(page)}
+      />
+    </>
+  );
 }
